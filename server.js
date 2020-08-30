983598f1cb0f7168f9eb475cd702df6d170b7aab
Lior Kasha <56375054+Dyn4sty@users.noreply.github.com>
2020-08-30 07:12:01 +0300

    updated the Content Security Policy

--- server.js
+++ server.js
@@ -13,7 +13,33 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "default-src": [
+          "'self'",
+          "https://*.gstatic.com",
+          "https://use.fontawesome.com",
+        ],
+        "script-src": [
+          "'self'",
+          "https://*.google.com",
+          "https://*.gstatic.com",
+          "https://use.fontawesome.com",
+        ],
+        "style-src": ["*"],
+        "img-src": ["*", "data:"],
+        "upgrade-insecure-requests": [],
+        "frame-src": [
+          "'self'",
+          "https://*.google.com",
+          "https://fashionorb-f7827.firebaseapp.com/",
+        ],
+      },
+    },
+  })
+);
 app.use(cors());
 app.use(compression());
 
